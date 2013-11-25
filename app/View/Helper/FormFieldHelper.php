840e45910c767a15d4d2c476e04a0556ce77cd01
Lyndsy Simon <lyndsy@lyndsysimon.com>
2013-11-25 15:09:55 -0500

    Cleans up FormFieldHelper

--- app/View/Helper/FormFieldHelper.php
+++ app/View/Helper/FormFieldHelper.php
@@ -11,30 +11,8 @@ class FormFieldHelper extends AppHelper {
 
     }
 
-    private function inputDefaults($helpText, $labelText) {
-        $after = '';
-        // if($helpText !== null) {
-        //     $after .= "<span class='help-block'>$helpText</span>";
-        // }
-        $after .= '</div></div>';
-
-        $label = array(
-            'text' => $labelText,
-            'class' => 'control-label'
-        );
-
-        return array(
-            'div' => false,
-            'placeholder' => false,
-            'before' => "<div class='control-group'>",
-            'between' => "<div class='controls'>",
-            'after' => $after,
-            'label' => $label,
-            'data-content' => $helpText
-        );
-    }
-
-    private function makeCommonOptions($labelText, $tip=null, $detailedTip=null) {
+    private function _default_options($labelText, $tip=null, $detailedTip=null) {
+        /* Returns a string-keyed array of options to pass to Form::input() */
         $options = array(
             'div' => false,
             'placeholder' => false,
@@ -69,6 +47,9 @@ class FormFieldHelper extends AppHelper {
     }
 
     public function textbox($options) {
+        /* Returns a regular textbox or a textarea, per Form:input()'s
+        introspection on the model field
+        */
         $strippedKeys = array('field', 'label', 'tip', 'detailedTip');
         foreach($strippedKeys as $k){
             $$k = isset($options[$k]) ? $options[$k] : null;
@@ -76,33 +57,13 @@ class FormFieldHelper extends AppHelper {
         }
 
         $options = array_merge(
-            $this->makeCommonOptions($label, $tip, $detailedTip),
+            $this->_default_options($label, $tip, $detailedTip),
             $options
         );
 
         return $this->Form->input($field, $options);
 
 
-    }
-
-    public function text($field, $labelText, $helpText=null, $rows=null, $class=null) {
-        /* Returns a Bootstrap, form-horizontal style text input field.
-
-        If $rows is specified, this returns a textarea instead. */
-
-        $options = $this->inputDefaults($helpText, $labelText);
-
-        if($rows !== null) {
-            $options['rows'] = $rows;
-        }
-
-        if($class !== null) {
-            $options['class'] = $class;
-        }
-
-        return $this->Form->input(
-            $field, $options
-        );
     }
 
     public function dropdownbox($options) {
@@ -113,41 +74,17 @@ class FormFieldHelper extends AppHelper {
         }
 
         $options = array_merge(
-            $this->makeCommonOptions($label, $tip, $detailedTip),
+            $this->_default_options($label, $tip, $detailedTip),
             $options
         );
 
         return $this->Form->input($field, $options);
     }
 
-    public function dropdown($field, $labelText, $options, $helpText=null, $class=null) {
-        return $this->select($field, $labelText, $options, $helpText, null, $class);
-    }
-
-    public function multi($field, $labelText, $options, $helpText=null, $class=null) {
-        return $this->select($field, $labelText, $options, $helpText, true, $class);
-    }
-
     public function checkboxes($options) {
         return $this->dropdownbox(
             array_merge($options, array('multiple' => 'checkbox'))
         );
-    }
-
-    private function select($field, $labelText, $options, $helpText, $multi=False, $class=null) {
-        $helpHtml = $this->generateHelpSpan($helpText);
-
-        // Add options for selection
-
-        $params = array_merge(
-            $this->inputDefaults($helpText, $labelText),
-            array('options' => $options)
-        );
-
-        $params['multiple'] = $multi;
-        $params['class'] = $class;
-
-        return $this->Form->input($field, $params);
     }
 
     public function inputGroupStart($options=null) {

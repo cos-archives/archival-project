f3c72f4e135889e5b68cee82d9ed3cdf3e00fc6f
Lyndsy Simon <lyndsy@lyndsysimon.com>
2013-11-25 18:00:51 -0500

    Remove references to the old coding form.

--- app/Controller/CodedpapersController.php
+++ app/Controller/CodedpapersController.php
@@ -41,7 +41,7 @@ class CodedpapersController extends AppController {
 		$this->Session->setFlash($insertcp['message'],$insertcp['alert']);
 		$cid = $insertcp['cid'];
 		if($cid !== null)
-			$this->redirect('code/'.$cid);
+			$this->redirect('entry/'.$cid);
 		else
 			$this->redirect('index/');
 		exit;
@@ -104,86 +104,8 @@ class CodedpapersController extends AppController {
 
 			$this->request->data = $this->Codedpaper->findDeep($id);
 		}
-
-
-
-
 	}
-	public function code ($id = NULL)
-	{
-		$this->Codedpaper->id = $id;
 
-		if (!$this->Codedpaper->exists())
-		    throw new NotFoundException('Invalid coded paper');
-
-		if ($this->request->is('post') OR $this->request->is('ajax'))
-		{
-			if($this->Codedpaper->saveAssociated($this->request->data, array("deep" => TRUE)	))
-			{
-				$msg = __('Study saved.');
-				$kind = 'alert-info';
-			}
-			else {
-				$msg = __('Study could not be saved!');
-				$kind = 'alert-error';
-			}
-		}
-
-		if(isset($msg) ) $this->Session->setFlash($msg,$kind);
-
-		$errors = array_unique(Set::flatten($this->Codedpaper->validationErrors));
-		if(!empty($errors))
-		{
-			function inc($matches) {
-			    return ++$matches[1];
-			}
-
-			foreach($errors AS $field => $error) {
-				$field =  preg_replace_callback( "|(\d+)|", "inc", $field);
-				$field = Inflector::humanize(str_replace("."," ",$field));
-
-				$msg .= "<br>". $field . ": ". $error;
-			}
-		}
-
-		if (!$this->request->is('ajax')) {
-			if(isset($msg) ) $this->Session->setFlash($msg,$kind);
-
-			### get data again (if I submitted abstract and title as hidden fields, I wouldn't need to do it)
-			$this->request->data = $this->Codedpaper->findDeep($id);
-
-			$replicatesStudyId = $this->Codedpaper->Study->getReplicable($id);
-
-			$this->set(compact('replicatesStudyId'));
-		}
-		else {
-			$this->set(compact('msg','kind'));
-			$this->render('message');
-		}
-	}
-	public function morestudies () {
-		$replicatesStudyId = $this->Codedpaper->Study->getReplicable();
-		$this->set(compact('replicatesStudyId'));
-
-		$this->request->data = $this->Codedpaper->Study->createDummy($this->request->query['codedpaper_id'], $this->request->query['sstart']);
-
-		if (!$this->request->is('ajax')) {
-			$this->Session->setFlash('Please enable JS.','alert-info');
-			$this->redirect('code/'.$this->request->query['codedpaper_id']);
-		}
-	}
-	public function moretests () {
-		$this->request->data = $this->Codedpaper->Study->Test->createDummy(
-			array( 'id' => $this->request->query['study_id'] ),
-			$this->request->query['s'],
-			$this->request->query['tstart']
-		);
-
-		if (!$this->request->is('ajax')) {
-			$this->Session->setFlash('Please enable JS.','alert-info');
-			$this->redirect('code/'.$this->request->query['codedpaper_id']);
-		}
-	}
 	public function compare ($id1 = NULL, $id2 = NULL) {
 		if (!$this->Codedpaper->exists($id1))
 		    throw new NotFoundException('First paper does not exist.');
@@ -207,13 +129,13 @@ class CodedpapersController extends AppController {
 		}
 		$this->request->data = $this->Codedpaper->findDeep($id);
 
-		$replicatesStudyId = $this->Codedpaper->Study->getReplicable($id);
+		$referenced_papers = $this->Codedpaper->Study->getReplicable($id);
 
 		$onlyView = true;
 
-		$this->set(compact('replicatesStudyId','onlyView'));
+		$this->set(compact('referenced_papers','onlyView'));
 
-		$this -> render('code'); ## added a couple of hooks in code.ctp
+		$this -> render('entry'); ## added a couple of hooks in code.ctp
 	}
 	public function index_mine() {
 		$this->set('codedpapers', $this->Codedpaper->find('all',
